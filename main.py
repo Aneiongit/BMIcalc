67608d31e9efca394fedf3cdb69f79d598cb7826
Anna M <83anei@gmail.com>
2021-10-06 18:59:26 +0100

    Updated

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-# Basal Metabolic Rate calculation
-def BMRW():
-    BMRW = 10 * weight + 6.25 * height - 5 * age - 161  # for women
 
-def BMRM():
-    BMRM = 10 * weight + 6.25 * height - 5 * age + 5  # for men
+
 
 # title
 print('\nCalorie calculator :)\n')
@@ -13,6 +9,17 @@ height = int(input('Please provide your height in centimeters: '))
 weight = int(input('Please provide your weight in kilograms: '))
 age = int(input('Please provide your age: '))
 
+
+# Basal Metabolic Rate calculation
+def BMRW():
+    BMRW = 10 * weight + 6.25 * height - 5 * age - 161  # for women
+    print(f"Your daily calories intake: {BMRW}")
+
+def BMRM():
+    BMRM = 10 * weight + 6.25 * height - 5 * age + 5  # for men
+    print(f"Your daily calories intake: {BMRM}")
+
+
 # BMI calculation
 BMI = (weight/((height/100)**2))
 print('\nYour Body Mass Index (BMI) is: ', round(BMI, 2), "and you are "  .format(BMI), end="")
@@ -20,16 +27,15 @@ print('\nYour Body Mass Index (BMI) is: ', round(BMI, 2), "and you are "  .forma
 # BMI description
 if BMI < 18:
     print(" underweight.")
-elif BMI >= 18.5 and BMI < 25:
+elif 18.5 <= BMI < 25:
     print(" healthy.")
-elif BMI >= 25 and BMI < 30:
+elif 25 >= BMI < 30:
     print(" overweight.")
 elif BMI >= 30:
     print(" obese.")
 
-
-print(BMRW())
-print(BMRM())
+BMRW()
+BMRM()
 
 # opcja trybu zycia do obliczenia zapotrzebowania kalorycznego
 print('\nAby obliczyc zapotrzebowanie, wybierz jeden z ponizszych trybow zycia: ')
@@ -42,8 +48,8 @@ print('2.0: Praca fizyczna ciężka, regularne ćwiczenia 7razy w tygodniu')
 tryb = input('\nWybierz tryb zycia: ')
 
 # obliczanie zapotrzebowania kalorycznego dla mezczyzny i kobiety
-zapotrzebowanieK = (BMRW*float(tryb))
-zapotrzebowanieM = (BMRM*float(tryb))
+zapotrzebowanieK = (BMRW()*float(tryb))
+zapotrzebowanieM = (BMRM()*float(tryb))
 print("\nDziennie zapotrzebowanie kaloryczne dla kobiety:", zapotrzebowanieK, 'kalorii.')
 print("\nDziennie zapotrzebowanie kaloryczne dla mezczyzny:", zapotrzebowanieM, 'kalorii.')
 
